069e457c31dc524bd93b1e85b7aa87d936456616
Bruno <brunohdzv@gmail.com>
2023-01-18 19:51:11 -0600

    se añaden rutas para login e index

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import * as page from './pages/index';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<page.Login />} />
+				<Route path='/secret' element={<page.Landing />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
