60d28f98e102d7a67ef8f1cf40ae6c28ec595abc
Bruno <brunohdzv@gmail.com>
2023-01-18 20:14:25 -0600

    se añade musica y tipografia

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div>
+			<p className='font-rachelya text-7xl'>Page</p>
+		</div>
+	);
 };
 
 export default Login;
